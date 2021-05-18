bd33887f7de4af8f79dab995557025303491171c
Vivek J <vivekj.cloud@gmail.com>
2021-05-18 02:44:34 +0000

    changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Again, this is coming from  a commit change !" );
+        System.out.println( "Hello World, this is coming from  a commit change !" );
     }
 }
